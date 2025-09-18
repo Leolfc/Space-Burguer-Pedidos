83ab0116f26dcdb58a7c1de9d4edf5508c88ac9f
Leonardo Feliciano <147149946+Leolfc@users.noreply.github.com>
2025-09-18 14:00:50 -0300

    Update script_final.js

--- script_final.js
+++ script_final.js
@@ -1658,7 +1658,7 @@ function checkRestaurantOpen() {
   const totalMinutes = hours * 60 + minutes;
   const abre = 18 * 60 + 30;
   const fecha = 23 * 60;
-  if (dia === 3) {
+  if (dia === 2) {
     return false;
   }
   return totalMinutes >= abre && totalMinutes <= fecha;
