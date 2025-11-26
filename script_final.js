50ac634c17eac57269a9e28ccf505a5bf0144b7c
Leonardo Feliciano <147149946+Leolfc@users.noreply.github.com>
2025-11-26 18:36:34 -0300

    Atualizar o script_final.js

--- script_final.js
+++ script_final.js
@@ -1771,7 +1771,7 @@ function checkRestaurantOpen() {
   const abre = 18 * 60 + 30;
   let fecha = 23 * 60; //horário padrão de fechamento finais de semana
 
-  if (dia === 2) {
+  if (dia === 3) {
     return false;
   }
   if (dia === 1 || dia === 3 || dia === 4) {
