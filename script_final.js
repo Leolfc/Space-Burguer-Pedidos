b52b77f14a52a89b44fb474c089940470162a373
Leonardo Feliciano <147149946+Leolfc@users.noreply.github.com>
2025-12-18 22:45:40 -0300

    Atualizar o script_final.js

--- script_final.js
+++ script_final.js
@@ -1775,7 +1775,7 @@ function checkRestaurantOpen() {
     return false;
   }
   if (dia === 1 || dia === 3 || dia === 4) {
-    fecha = 23 * 60  ; // horário de fechamento de segunda, quarta e quinta(fecha mais cedo)
+    fecha = 22 * 60  ; // horário de fechamento de segunda, quarta e quinta(fecha mais cedo)
   }
   return totalMinutes >= abre && totalMinutes <= fecha;
 }
