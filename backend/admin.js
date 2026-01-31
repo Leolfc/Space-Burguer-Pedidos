f208699171d33edf7c87bf3793871f9c37cbd8a2
Leolfc <leonardofeliciano81@gmail.com>
2026-01-31 13:40:16 -0300

    alterando o painel 03

--- backend/admin.js
+++ backend/admin.js
@@ -196,7 +196,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function fazerLogout() {
     sessionStorage.removeItem("isLoggedIn");
-    window.location.href = "/login";
+    window.location.href = "login";
   }
 
   async function verificarStatusLoja() {
